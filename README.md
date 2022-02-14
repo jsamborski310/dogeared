5e5b61c9f2d0e20e147ea7525df757d49c7b896f
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-02-13 20:59:18 -0800

    Updated server.js. Seeds.

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-# dogeared
-=======
 # dogeared
 
 hey
->>>>>>> ab47ca3142a38831e5fd5149079cfb70ce80471b
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
@@ -9,12 +10,13 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 86400,
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({

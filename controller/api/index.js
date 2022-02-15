3b00aa269807e2d71825d8ac58fac5962f0f9a94
Mohamed Mohamud <momo206@uw.edu>
2022-02-14 18:49:56 -0800

    indexRoutesSetup

---
+++ controller/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const notesRoutes = require('./notesRoutes');
+const bookRoutes = require('./bookRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/notes', notesRoutes);
+router.use('/book', bookRoutes);
+router.use('/user', userRoutes);
+
+module.exports = router;

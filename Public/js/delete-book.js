e9c1a41061b58f59cca16c0c902831f446c75ecf
Christina Bohn <christinajbohn@gmail.com>
2022-02-25 10:18:14 -0800

    Fixed delete button on book details page.

--- Public/js/delete-book.js
+++ Public/js/delete-book.js
@@ -2,21 +2,23 @@
 const deleteBook = async (event) => {
     // event.preventDefault();
   
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
+    if (event.currentTarget.hasAttribute('data-id')) {
+      const id = event.currentTarget.getAttribute('data-id');
   
       const response = await fetch(`/api/books/${id}`, {
         method: 'DELETE',
       });
   
+
       if (response.ok) {
         document.location.replace('/');
       } else {
         alert('Failed to delete book');
       }
     }
+
   };
 
 document
-.querySelector('.delete-book')
-.addEventListener('submit', deleteBook);
+.querySelector('.delete-button')
+.addEventListener('click', deleteBook);
